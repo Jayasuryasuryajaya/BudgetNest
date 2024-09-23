c7fb097e63a9ecee6b120dcf3cbe612795e7e3bf
jackw1-tech <107569512+jackw1-tech@users.noreply.github.com>
2024-09-23 16:46:18 +0200

    new Start

--- Piattaforma/Piattaforma/urls.py
+++ Piattaforma/Piattaforma/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for progetto1 project.
+URL configuration for Piattaforma project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -15,12 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from App1 import urls as rotte
+from django.urls import path
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path ("", include(rotte) ),
-    
-    
 ]
-
--- Piattaforma/Piattaforma/wsgi.py
+++ Piattaforma/Piattaforma/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for progetto1 project.
+WSGI config for Piattaforma project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'progetto1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Piattaforma.settings')
 
 application = get_wsgi_application()
